02dc2855bbe6a704b5c32d4b7969a403b016f271
Jashandeep Singh <jsingh193@academic.rrc.ca>
2026-02-24 13:54:16 -0600

    Updagte Python Code

--- calculator.py
+++ calculator.py
@@ -1,55 +1,36 @@
-# This function adds two numbers
-def add(x, y):
-    return x + y
-
-# This function subtracts two numbers
-def subtract(x, y):
-    return x - y
-
-# This function multiplies two numbers
-def multiply(x, y):
-    return x * y
-
-# This function divides two numbers
-def divide(x, y):
-    return x / y
-
-
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
-
-while True:
-    # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
-
-    # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
-        try:
-            num1 = float(input("Enter first number: "))
-            num2 = float(input("Enter second number: "))
-        except ValueError:
-            print("Invalid input. Please enter a number.")
-            continue
-
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
-
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
-
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
-
-        elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
-        
-        # check if user wants another calculation
-        # break the while loop if answer is no
-        next_calculation = input("Let's do next calculation? (yes/no): ")
-        if next_calculation == "no":
-          break
-    else:
-        print("Invalid Input")
+import os
+import pymysql
+from urllib.request import urlopen
+ 
+db_config = {
+    'host': 'mydatabase.com',
+    'user': 'admin',
+    'password': 'secret1234'
+}
+ 
+def get_user_input():
+    user_input = input('Enter your name: ')
+    return user_input
+ 
+def send_email(to, subject, body):
+    os.system(f'echo {body} | mail -s "{subject}" {to}')
+ 
+def get_data():
+    url = 'http://insecure-api.com/get-data'
+    data = urlopen(url).read().decode()
+    return data
+ 
+def save_to_db(data):
+    query = f"INSERT INTO mytable (column1, column2) VALUES ('{data}', 'Another Value')"
+    connection = pymysql.connect(**db_config)
+    cursor = connection.cursor()
+    cursor.execute(query)
+    connection.commit()
+    cursor.close()
+    connection.close()
+ 
+if __name__ == '__main__':
+    user_input = get_user_input()
+    data = get_data()
+    save_to_db(data)
+    send_email('admin@example.com', 'User Input', user_input)
